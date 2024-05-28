cc41702d8255e333ffa987298ddefbf1d1bb3dda
simonanderson16 <uhx8bu@virginia.edu>
2024-05-28 15:46:22 -0400

    fill in projects info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState } from "react";
-import { Card } from "@nextui-org/react";
+import { Chip, Tooltip } from "@nextui-org/react";
 import "../styles/Projects.css";
 import info from "../info/projects.json";
+import { FaGithub } from "react-icons/fa";
+import { IoOpenOutline } from "react-icons/io5";
+import { SiDevpost } from "react-icons/si";
 
 const Projects = () => {
     const projects = info.projects;
@@ -15,16 +18,9 @@ const Projects = () => {
     return (
         <div id="projects" className="section-container projects-container">
             <h1 className="projects-header">Projects</h1>
-            {/* <div className="projects-grid">
-                {projects.map((project, index) => (
-                    <div className={`project-card project-${index + 1}`} key={index}>
-                        <div className="project-card-front">
-                            <p className="project-title">{project.title}</p>
-                        </div>
-                        <div className="project-card-back">Description</div>
-                    </div>
-                ))}
-            </div> */}
+            <Chip variant="flat" className="projects-chip">
+                Click on each project to learn more
+            </Chip>
             <div className="projects-grid">
                 {projects.map((project, index) => (
                     <div
@@ -34,10 +30,28 @@ const Projects = () => {
                     >
                         <div className="project-card-inner">
                             <div className="project-card-front">
+                                <img src={project.image} alt={project.title} className="project-image" />
                                 <p className="project-title">{project.title}</p>
                             </div>
                             <div className="project-card-back">
-                                <p>Description of {project.title}</p>
+                                <div className="card-back-content">
+                                    <p>{project.description}</p>
+                                    <div className="project-links">
+                                        {project.links.map((link, index) => (
+                                            <Tooltip key={index} content={link.name} showArrow color="secondary" size="sm" closeDelay={0}>
+                                                <a href={link.url} target="_blank" rel="noreferrer" className="project-link">
+                                                    {link.name === "GitHub" ? (
+                                                        <FaGithub className="project-icon" />
+                                                    ) : link.name === "Visit" ? (
+                                                        <IoOpenOutline className="project-icon" />
+                                                    ) : (
+                                                        <SiDevpost className="project-icon" />
+                                                    )}
+                                                </a>
+                                            </Tooltip>
+                                        ))}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
